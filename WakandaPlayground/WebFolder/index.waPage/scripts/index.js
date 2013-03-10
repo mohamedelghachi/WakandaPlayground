03f44aedac443a01f8115e76112d79bfe9951979
Greg McCarvell <greg@gregmccarvell.com>
2013-03-10 10:48:36 -0400

    First iteration complete for download

--- WakandaPlayground/WebFolder/index.waPage/scripts/index.js
+++ WakandaPlayground/WebFolder/index.waPage/scripts/index.js
@@ -2,6 +2,7 @@
 WAF.onAfterInit = function onAfterInit() {// @lock
 
 // @region namespaceDeclaration// @startlock
+	var searchGrid = {};	// @dataGrid
 	var searchInput = {};	// @textField
 	var clearDataButton = {};	// @button
 	var generateDataButton = {};	// @button
@@ -9,6 +10,11 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 // @endregion// @endlock
 
 // eventHandlers// @lock
+
+	searchGrid.onRowClick = function searchGrid_onRowClick (event)// @startlock
+	{// @endlock
+		sources.companyList.selectByKey(sources.companySearch.ID);
+	};// @lock
 
 	searchInput.keyup = function searchInput_keyup (event)// @startlock
 	{// @endlock
@@ -19,18 +25,22 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 	{// @endlock
 		ds.Person.removeData({
 			onSuccess: function(event){
-				sources.company.all();
+				sources.companyList.all();
 			}
 		});
 	};// @lock
 
 	generateDataButton.click = function generateDataButton_click (event)// @startlock
 	{// @endlock
-		ds.Person.createSampleData($$('numberOfPeopleInput').getValue(), {
-			onSuccess: function(event){
-				sources.companyList.all();
-			}
-		});
+		if($$('numberOfPeopleInput').getValue().length > 0){
+			ds.Person.createSampleData($$('numberOfPeopleInput').getValue(), {
+				onSuccess: function(event){
+					sources.companyList.all();
+				}
+			});
+		} else {
+			alert('Enter the number of people to generate');
+		}
 	};// @lock
 
 	documentEvent.onLoad = function documentEvent_onLoad (event)// @startlock
@@ -39,6 +49,7 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 	};// @lock
 
 // @region eventManager// @startlock
+	WAF.addListener("searchGrid", "onRowClick", searchGrid.onRowClick, "WAF");
 	WAF.addListener("searchInput", "keyup", searchInput.keyup, "WAF");
 	WAF.addListener("clearDataButton", "click", clearDataButton.click, "WAF");
 	WAF.addListener("generateDataButton", "click", generateDataButton.click, "WAF");
