ad12711431396235e7d4f4959932a21a3d2c6963
Greg McCarvell <greg@gregmccarvell.com>
2013-03-09 21:52:26 -0500

    Started index layout

---
+++ WakandaPlayground/WebFolder/index.waPage/scripts/index.js
@@ -0,0 +1,18 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var documentEvent = {};	// @document
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	documentEvent.onLoad = function documentEvent_onLoad (event)// @startlock
+	{// @endlock
+		sources.companyList.all();
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("document", "onLoad", documentEvent.onLoad, "WAF");
+// @endregion
+};// @endlock
