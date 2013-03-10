de1e88e691da8789784f2fb4dbd058067ec6d028
Greg McCarvell <greg@gregmccarvell.com>
2013-03-10 10:28:09 -0400

    Added generate and clear button scripts + UI

--- WakandaPlayground/WebFolder/index.waPage/scripts/index.js
+++ WakandaPlayground/WebFolder/index.waPage/scripts/index.js
@@ -2,10 +2,32 @@
 WAF.onAfterInit = function onAfterInit() {// @lock
 
 // @region namespaceDeclaration// @startlock
+	var clearDataButton = {};	// @button
+	var generateDataButton = {};	// @button
 	var documentEvent = {};	// @document
 // @endregion// @endlock
 
 // eventHandlers// @lock
+
+	clearDataButton.click = function clearDataButton_click (event)// @startlock
+	{// @endlock
+		ds.Person.removeData({
+			onSuccess: function(event){
+				sources.person.all();
+				sources.company.all();
+			}
+		});
+	};// @lock
+
+	generateDataButton.click = function generateDataButton_click (event)// @startlock
+	{// @endlock
+		ds.Person.createSampleData($$('numberOfPeopleInput').getValue(), {
+			onSuccess: function(event){
+				sources.person.all();
+				sources.company.all();
+			}
+		});
+	};// @lock
 
 	documentEvent.onLoad = function documentEvent_onLoad (event)// @startlock
 	{// @endlock
@@ -13,6 +35,8 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 	};// @lock
 
 // @region eventManager// @startlock
+	WAF.addListener("clearDataButton", "click", clearDataButton.click, "WAF");
+	WAF.addListener("generateDataButton", "click", generateDataButton.click, "WAF");
 	WAF.addListener("document", "onLoad", documentEvent.onLoad, "WAF");
 // @endregion
 };// @endlock
